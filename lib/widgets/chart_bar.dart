390a244297d9e6e7ce1cb4f94f7e7cdf67e08366
davet <daveenq@gmail.com>
2020-02-13 11:04:50 +0000

    Used LayoutBuilder to get heights of the parent widget and size the child widgets so the layout is best for the size of the parent

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -13,47 +13,51 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        final curScaleFactor = MediaQuery.of(context).textScaleFactor;
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-          child:FittedBox(
-          child: Text('\£${spendingAmount.toStringAsFixed(0)}', style: TextStyle(fontSize: 18 * curScaleFactor,)),
-          ), 
-        ),
-        SizedBox(
-          height: 8,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spendingPctOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
+    final curScaleFactor = MediaQuery.of(context).textScaleFactor;
+    // constraint - how much height/ width a widget can occupy
+    return LayoutBuilder(builder: (ctx, constraints){
+      return Column(
+          children: <Widget>[
+            Container(
+              height: constraints.maxHeight * 0.2,
+              child:FittedBox(
+              child: Text('\£${spendingAmount.toStringAsFixed(0)}', style: TextStyle(fontSize: 18 * curScaleFactor,)),
+              ), 
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.5,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1.0),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ),
+                  FractionallySizedBox(
+                    heightFactor: spendingPctOfTotal,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(label,  style: TextStyle(fontSize: 16 * curScaleFactor,)),
-      ],
-    );
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Text(label,  style: TextStyle(fontSize: 16 * curScaleFactor,)),
+          ],
+       );
+    });
+    
   }
 }
